01f6221d203d8812e1a859f0a2b4899bb5a64457
Yeung2017 <879006948@qq.com>
2018-10-30 14:36:44 +0800

    add Page demo

---
+++ pages/Demo/index.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Demo extends Component {
+  render() {
+    return (
+      <View style={styles.demo}>
+        <Text>demo page</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  demo: {
+    color: 'red'
+  },
+});
